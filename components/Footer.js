364ee4c1c9c1b9fef4f47dad09870d25551ed8d1
Dj Walker-Morgan <codepope@gmail.com>
2021-12-13 15:58:47 +0000

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        Made with the hope that he'll won't be Prime Minister soon
       </footer>
     </>
   )
